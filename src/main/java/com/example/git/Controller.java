0fb0f465169d8f6f0be10cc98f1b9abfec130196
anatoly <kaim@mail.ru>
2018-04-16 15:31:17 +0200

    5

--- src/main/java/com/example/git/Controller.java
+++ src/main/java/com/example/git/Controller.java
@@ -1,9 +1,12 @@
 package com.example.git;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 //коммит на сервере
 @org.springframework.stereotype.Controller
 public class Controller {
 
+    @RequestMapping("/")
     public String myTabl() {
         return "tabl";
     }
