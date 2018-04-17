6d2990db135b52c2313d68dd34d78bbc4c27a128
anatoly <kaim@mail.ru>
2018-04-17 14:50:49 +0200

    8

--- src/main/java/com/example/git/Repositories.java
+++ src/main/java/com/example/git/Repositories.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Repositories extends JpaRepository<User, Integer> {
+public interface Repositories extends JpaRepository<Users, Integer> {
 }
